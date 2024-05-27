854033baf0a836ae926d6e8932f43701f37d6070
irfan <cakirfan.001@gmail.com>
2024-05-27 22:23:49 +0200

    new properties added

--- src/pages/autoDetails/core/DetailSection.jsx
+++ src/pages/autoDetails/core/DetailSection.jsx
@@ -5,17 +5,17 @@ const DetailSection = ({ details }) => (
     <h4 className="text-center mb-3">Details</h4>
     {details.map((detail, idx) => (
       <div>
-        <div key={idx} className="py-1 d-flex align-items-center">
+        <div key={idx} className="d-flex align-items-center gap-1">
           <FontAwesomeIcon icon={detail.icon} className="mx-2" />{" "}
           <div
-            className="w-100 fw-semibold d-flex justify-content-between text-uppercase"
+            className="w-100 fw-semibold d-flex justify-content-between text-capitalize"
             style={{ fontSize: ".9rem" }}
           >
             <span className="text-secondary">{detail.label1}</span>
             <span>{detail.label2}</span>
           </div>
         </div>
-        <hr />
+        <hr style={{marginBottom: ".7rem"}}/>
       </div>
     ))}
   </div>
