578158db71f6a2df1d9c2ba73fe6b38105e868aa
irfan <cakirfan.001@gmail.com>
2024-06-12 20:02:53 +0200

    bg image added to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,21 +4,30 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { Link } from "react-router-dom";
 
-const myNavbar = () => {
+const MyNavbar = () => {
   return (
-    <Navbar expand="lg" className="bg-success fixed-top">
+    <Navbar
+      expand="lg"
+      className="fixed-top"
+      style={{
+        backgroundImage:
+          'url("https://cdn.pixabay.com/photo/2013/11/08/17/58/cirrus-clouds-207534_640.jpg")',
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
       <Container>
         <Navbar.Brand>
           <Link to="/">
             <img
               src="https://cdn.pixabay.com/photo/2013/07/13/01/11/passenger-car-155267_640.png"
-              alt=""
+              alt="Brand Logo"
               width={100}
             />
           </Link>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav bg-danger">
+        <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto d-flex gap-3">
             <Nav.Link>
               <Link
@@ -29,7 +38,11 @@ const myNavbar = () => {
                 START
               </Link>
             </Nav.Link>
-            <NavDropdown title="FAHRZEUGE" id="basic-nav-dropdown">
+            <NavDropdown
+              title="FAHRZEUGE"
+              id="basic-nav-dropdown"
+              className="text-white"
+            >
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
@@ -40,8 +53,14 @@ const myNavbar = () => {
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#link">GEWERBEKUNDEN</Nav.Link>
-            <NavDropdown title="SERVICE & ZUBEHÖR" id="basic-nav-dropdown">
+            <Nav.Link className="text-white" href="#link">
+              GEWERBEKUNDEN
+            </Nav.Link>
+            <NavDropdown
+              title="SERVICE & ZUBEHÖR"
+              id="basic-nav-dropdown"
+              className="text-white"
+            >
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
@@ -52,7 +71,11 @@ const myNavbar = () => {
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
-            <NavDropdown title="UNTERNEHMEN" id="basic-nav-dropdown">
+            <NavDropdown
+              title="UNTERNEHMEN"
+              id="basic-nav-dropdown"
+              className="text-white"
+            >
               <NavDropdown.Item href="#action/3.1">Über Uns</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.2">Karriere</NavDropdown.Item>
@@ -73,4 +96,4 @@ const myNavbar = () => {
   );
 };
 
-export default myNavbar;
+export default MyNavbar;
