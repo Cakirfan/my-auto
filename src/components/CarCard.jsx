c0b4d144a0db50bc6e1bdd18c94d4ed794f4f903
irfan <cakirfan.001@gmail.com>
2024-06-12 20:25:12 +0200

    CarCard component created and added to Home

---
+++ src/components/CarCard.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import { cars } from "../helper/data";
+
+const CarCard = ({ car }) => {
+  return (
+    <Card className="mb-3">
+      <Card.Img variant="top" src={car.images[0]} style={{ height: "15rem" }} />
+      <Card.Body>
+        <div className="d-flex gap-2">
+          <Card.Text className="fs-5 fw-semibold text-uppercase">{car.marke}</Card.Text>
+          <Card.Text className="fs-5 text-secondary">{car.model}</Card.Text>
+        </div>
+
+        <Card.Text>{car.details[0].value}</Card.Text>
+      </Card.Body>
+      <Card.Footer className="text-center fs-4 fw-semibold">
+        <small className="text-muted">{car.preis} €</small>
+      </Card.Footer>
+    </Card>
+  );
+};
+
+const LatestCars = () => {
+  // Sortiere die Autos nach dem Datum (umgekehrte Reihenfolge, um die neuesten zuerst zu erhalten)
+  const sortedCars = [...cars].sort(
+    (a, b) => new Date(b.datum) - new Date(a.datum)
+  );
+  // Filtere die neuesten 8 Autos
+  const latestCars = sortedCars.slice(0, 8);
+
+  return (
+    <Container className="mt-5">
+      <h2>Neueste Autos</h2>
+      <Row className="row-cols-1 row-cols-md-3 row-cols-lg-4 g-4 mt-3">
+        {latestCars.map((car, index) => (
+          <Col key={index} xl={3} lg={4} md={6} sm={12}>
+            <CarCard car={car} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
+
+export default LatestCars;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,20 @@
-import Angebote from "../components/carAngebote/Angebote"
+// import Angebote from "../components/carAngebote/Angebote"
 import CarouselTop from "../components/CarouselTop"
 import FahrzeugSuchen from "../components/FahrzeugSuchen"
 import Finanzierung from "../components/Finanzierung"
+import LatestCars from "../components/CarCard"
 
 
 const Home = () => {
   return (
     <>
-      <CarouselTop/>
-      <FahrzeugSuchen/>
-      <Finanzierung/>
-      <Angebote/>
+      <CarouselTop />
+      <FahrzeugSuchen />
+      <Finanzierung />
+      {/* <Angebote/> */}
+      <LatestCars/>
     </>
-  )
+  );
 }
 
 export default Home
